888e27b788853464780d507fb3bedeea6c91c9fb
Mark Murray <mark.murray.ie@gmail.com>
2018-03-19 23:53:00 +0000

    better danger output

--- dangerfile.js
+++ dangerfile.js
@@ -1,20 +1,20 @@
 const jest = require('danger-plugin-jest').default;
-const { message, danger, warn, markdown } = require('danger');
+const { message, danger, warn } = require('danger');
 
 // Output all modified files in the PR
-const modifiedMD = danger.git.modified_files.join('- ');
-message('Changed Files in this PR: \n - ' + modifiedMD);
+const modified = danger.git.modified_files;
+message('Changed Files in this PR: \n' + modified.map(file => `- ${file}\n`));
 
 // Encourage smaller PRs
 let errorCount = 0;
 const bigPRThreshold = 600;
+const thresholdErrorMessage =
+  'Pull Request size seems quite large. If the PR contains multiple changes, try splitting each change into a separate PR to aid faster, easier reviews.';
 if (danger.github.pr.additions + danger.github.pr.deletions > bigPRThreshold) {
-  warn(':exclamation: Big PR (' + ++errorCount + ')');
-  markdown(
-    '> (' +
-      errorCount +
-      ') : Pull Request size seems quite large. If the PR contains multiple changes, try splitting each change into a separate PR to aid faster, easier reviews.'
+  warn(
+    `:exclamation: PR is too big (${++errorCount}) - ${thresholdErrorMessage}`
   );
 }
 
+// Run jest test suites
 jest();

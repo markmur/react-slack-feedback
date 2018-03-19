13c1e347f6b212a026ae42acceba26b0f895dca4
Mark Murray <mark.murray.ie@gmail.com>
2018-03-19 23:20:52 +0000

    add code coverage

--- dangerfile.js
+++ dangerfile.js
@@ -1,11 +1,7 @@
-const path = require('path');
 const jest = require('danger-plugin-jest');
 const { message, danger } = require('danger');
 
 const modifiedMD = danger.git.modified_files.join('- ');
 message('Changed Files in this PR: \n - ' + modifiedMD);
 
-// Default
 jest();
-// Custom path
-jest({ testResultsJsonPath: path.resolve(__dirname, 'tests/results.json') });

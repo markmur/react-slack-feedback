3f902e173fe3bc73c04359d4e011a2a67fc6eec5
Mark Murray <mark.murray.ie@gmail.com>
2018-03-19 23:44:17 +0000

    encourage smaller prs via dangerfile

--- dangerfile.js
+++ dangerfile.js
@@ -1,7 +1,20 @@
 const jest = require('danger-plugin-jest').default;
-const { message, danger } = require('danger');
+const { message, danger, warn, markdown } = require('danger');
 
+// Output all modified files in the PR
 const modifiedMD = danger.git.modified_files.join('- ');
 message('Changed Files in this PR: \n - ' + modifiedMD);
+
+// Encourage smaller PRs
+let errorCount = 0;
+const bigPRThreshold = 600;
+if (danger.github.pr.additions + danger.github.pr.deletions > bigPRThreshold) {
+  warn(':exclamation: Big PR (' + ++errorCount + ')');
+  markdown(
+    '> (' +
+      errorCount +
+      ') : Pull Request size seems quite large. If the PR contains multiple changes, try splitting each change into a separate PR to aid faster, easier reviews.'
+  );
+}
 
 jest();

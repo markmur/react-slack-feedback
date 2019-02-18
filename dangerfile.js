a72aa657048c2d5b660995738b0719336546175f
Mark Murray <mark.murray.ie@gmail.com>
2019-02-18 22:19:22 +0000

    Remove jest from danger

--- dangerfile.js
+++ dangerfile.js
@@ -1,10 +1,5 @@
-const jest = require('danger-plugin-jest').default
-
 const { message, danger } = require('danger')
 
 // Output all modified files in the PR
 const modified = danger.git.modified_files
 message('Files changed in this PR: \n' + modified.map(file => `\n- ${file}`))
-
-// Run jest test suites
-jest()

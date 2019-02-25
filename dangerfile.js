e195ba0b16d65590ecdc648a7f7641144bd585fe
Mark Murray <mark.murray.ie@gmail.com>
2019-02-25 17:55:01 +0000

    Remove danger

--- dangerfile.js
+++
@@ -1,5 +0,0 @@
-const { message, danger } = require('danger')
-
-// Output all modified files in the PR
-const modified = danger.git.modified_files
-message('Files changed in this PR: \n' + modified.map(file => `\n- ${file}`))
